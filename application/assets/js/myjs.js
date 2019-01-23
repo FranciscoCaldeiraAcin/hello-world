1c31ae0115b17ab78439c820307a1041568af181
FranciscoCaldeiraAcin <francisco.caldeira@acin.pt>
2019-01-23 14:50:48 +0000

    Novas regras por métodos

--- application/assets/js/myjs.js
+++ application/assets/js/myjs.js
@@ -6,13 +6,23 @@
 //add new script <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
                                 //AULA4
+
 $(document).ready(function(){ 
+//https://jqueryvalidation.org/   >documentation
+
+    //método criado para regra(rule)
+    $.validator.addMethod("meow", function(value, element) {
+        return /^A/.test( value );
+    }, 'Please enter a A in the beginning.');
 
     $('form').validate({
         rules: {
-            //NAME do input
+            //NAME do input no html
             fullname: {
               required: true,
+              minlength: 3,
+              maxlength: 10,
+              meow: true, //regra nova, escrita em método
             },
             // email: {
             //     required: true,
@@ -20,14 +30,12 @@ $(document).ready(function(){
             // },
             password: {
                 required: true,
-                email: true
             },
-
         }
 
       });
 
- });
+});
     
 
 
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,128 +5,116 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
-	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-62+JPIF7fVYAPS4itRiqKa7VU321chxfKZRtkSY0tGoTwcUItAFEH/HGTpvDH6e6" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-	<script type='text/javascript' language="javascript" src="<?php echo base_url(); ?>application/assets/js/myjs.js"></script>
-  <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
-  <style>
-		body{
-			margin:30px;
-		}
-	</style>
+	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-62+JPIF7fVYAPS4itRiqKa7VU321chxfKZRtkSY0tGoTwcUItAFEH/HGTpvDH6e6" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>application/assets/css/mycss.css">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script type='text/javascript' language="javascript" src="<?php echo base_url(); ?>application/assets/js/myjs.js"></script>
+  <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script> <!--PLUGIN https://jqueryvalidation.org/ -->
+
 </head>
 <body>
 
 <form>
       
-        <!-- <legend>Legend</legend>
-        <div class="form-group row">
-          <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com">
-          </div>
-        </div> -->
+	<div class="form-group">
+    <label for="fullname">Name</label>
+    <input type="text" id="fullname" name="fullname" class="form-control">
+  </div>
 
-		<div class="form-group">
-          <label for="fullname">Name</label>
-          <input type="text" id="fullname" name="fullname" class="form-control">
-        </div>
+  <!--<div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+  </div> -->
 
-        <!-- <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
-        </div> -->
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+  </div>
 
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
-        </div> 
+	<div class="form-group">
+    <label for="exampleTextarea">Example textarea</label>
+    <textarea class="form-control" id="exampleTextarea" rows="3" name="textarea"></textarea>
+  </div>
 
-		<div class="form-group">
-          <label for="exampleTextarea">Example textarea</label>
-          <textarea class="form-control" id="exampleTextarea" rows="3" name="textarea"></textarea>
-        </div>
+	<div class="form-group"> 
+    <label for="exampleInputFile">File input</label>
+    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+    <small id="fileHelp" class="form-text text-muted">not more thatn 25mb.</small>
+  </div>
 
-		<div class="form-group"> 
-          <label for="exampleInputFile">File input</label>
-          <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
-          <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
-    </div>
-
-        <div class="form-group">
-          <label for="exampleSelect1">Example select</label>
-          <select class="form-control" id="exampleSelect1" name="optionsSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
+  <div class="form-group">
+    <label for="exampleSelect1">Example select</label>
+    <select class="form-control" id="exampleSelect1" name="optionsSelect">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
 
 	<!-- rectangulos -->
-		
-		  <div class="form-check">
-        <label class="form-check-label">
-          <input class="form-group-input" type="checkbox" name="optionsCheckbox" value="1">
-          Option 1
-        </label>
-      </div>
+	<div class="form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox" name="optionsCheckbox" value="1">
+      Option 1
+    </label>
+  </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox" name="optionsCheckbox" value="2" checked="">
+      Option 2
+    </label>
+  </div>
+  <div class="form-check disabled">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox" name="optionsCheckbox" value="3" disabled="">
+      Option 3
+    </label>
+  </div>
+	
+  <br>
 
-      <div class="form-check">
-        <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" name="optionsCheckbox" value="2" checked="">
-          Option 2
-        </label>
-      </div>
+  <!-- bolas --> 
+	<div class="form-check">
+    <label class="form-check-label">
+      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+      Option one 
+    </label>
+  </div>
 
-      <div class="form-check disabled">
-        <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" name="optionsCheckbox" value="3" disabled="">
-          Option 3
-        </label>
-      </div>
-	 
-      <br>
-        <!-- bolas -->
-        
-		  <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-          Option one 
-        </label>
-      </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+      Option two 
+    </label>
+  </div>
 
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
-          Option two 
-        </label>
-      </div>
+  <div class="form-check disabled">
+    <label class="form-check-label">
+      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled="">
+      Option three 
+    </label>
+  </div>
+    
+  <!-- <div class="form-group">
+    <label for="exampleSelect2">Example multiple select</label>
+    <select multiple="" class="form-control" id="exampleSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div> -->
 
-      <div class="form-check disabled">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled="">
-          Option three 
-        </label>
-      </div>
-      
-        <!-- <div class="form-group">
-          <label for="exampleSelect2">Example multiple select</label>
-          <select multiple="" class="form-control" id="exampleSelect2">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div> -->
-
-        <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 
 </form>
-<!-- AULA3 -->
+
+<!-- AULA3 S/CI -->
 <!-- <script>
 	$(':text').css('border','3px solid red');
 	$(':password').css('border','3px solid black');
